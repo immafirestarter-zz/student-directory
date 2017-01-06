ac3adeb4010b1962b4283ef5e279f832fe8a3daa
Sami <sami7mason@yahoo.co.uk>
2017-01-06 15:38:50 +0000

    count added

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
 puts "The Students of Villains Academy"
 puts "------------------------------"
+# list of students
 puts "Dr. Hannibal Lecter"
 puts "Darth Vader"
 puts "John Doe"
@@ -10,3 +11,8 @@ puts "Colonel Hans Landa"
 puts "Hans Gruber"
 puts "Agent Smith"
 puts "Saruman"
+puts "Cersei Lannister"
+# student count
+print "Overall, there are "
+print 11
+print " eeeevillllll students"
