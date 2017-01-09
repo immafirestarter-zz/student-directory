1b642d894edefb737bbe9d9864d3c7255d0c33fe
Sami <sami7mason@yahoo.co.uk>
2017-01-09 02:00:23 +0000

    hashes created

--- directory.rb
+++ directory.rb
@@ -1,30 +1,30 @@
-# array of students
+# converted array to hashes of students
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "John Doe",
-  "Verbal Kint",
-  "Nurse Ratched",
-  "The Joker",
-  "Colonel Hans Landa",
-  "Hans Gruber",
-  "Agent Smith",
-  "Saruman",
-  "Cersei Lannister"
+  {name: "Dr. Hannibal Lecter", cohort: :march},
+  {name: "Darth Vader", cohort: :march},
+  {name: "John Doe", cohort: :november},
+  {name: "Verbal Kint", cohort: :november},
+  {name: "Nurse Ratched", cohort: :march},
+  {name: "The Joker", cohort: :november},
+  {name: "Colonel Hans Landa", cohort: :november},
+  {name: "Hans Gruber", cohort: :november},
+  {name: "Agent Smith", cohort: :march},
+  {name: "Saruman", cohort: :march},
+  {name: "Cersei Lannister", cohort: :march}
 ]
 def print_header
 puts "The Students of Villains Academy"
 puts "------------------------------"
 end
 
-def print(names)
-names.each do |name|
-  puts name
+def print(students)
+students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 
-def print_footer(names)
-puts "There are exactly #{names.count} eeeevilll students."
+def print_footer(students)
+puts "There are exactly #{students.count} eeeevilll students."
 end
 #now to call methods
 print_header
