8e28212ddb7ceeb9965eca82b56555fe73867597
Sami <sami7mason@yahoo.co.uk>
2017-01-10 07:32:43 +0000

    user interactivity added

--- directory.rb
+++ directory.rb
@@ -1,17 +1,24 @@
-# converted array to hashes of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :march},
-  {name: "Darth Vader", cohort: :march},
-  {name: "John Doe", cohort: :november},
-  {name: "Verbal Kint", cohort: :november},
-  {name: "Nurse Ratched", cohort: :march},
-  {name: "The Joker", cohort: :november},
-  {name: "Colonel Hans Landa", cohort: :november},
-  {name: "Hans Gruber", cohort: :november},
-  {name: "Agent Smith", cohort: :march},
-  {name: "Saruman", cohort: :march},
-  {name: "Cersei Lannister", cohort: :march}
-]
+#method for collecting sudent data from using and adding to hash.
+def student_input
+
+  puts "PLease enter the names of the enrolling students:"
+  puts "To finish, press enter twice"
+  #now for the empty array
+  students = []
+  #for first name
+  name = gets.chomp
+  #while name isn't empty, repeat.
+  while !name.empty? do
+    #now to add student hashe
+    students << {name: name, cohort: :march}
+    puts "Now we have #{students.count} eeeevillll students"
+    #gets another name
+    name = gets.chomp
+  end
+  #return array
+  students
+end
+
 def print_header
 puts "The Students of Villains Academy"
 puts "------------------------------"
@@ -27,6 +34,7 @@ def print_footer(students)
 puts "There are exactly #{students.count} eeeevilll students."
 end
 #now to call methods
+students = student_input
 print_header
 print(students)
 puts
