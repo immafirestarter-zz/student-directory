f867dda44b5c98760b5177e60d014a03cb7f9e27
Sami <sami7mason@yahoo.co.uk>
2017-01-14 22:38:30 +0000

    ex3

---
+++ ex8/directoryex3.rb
@@ -0,0 +1,45 @@
+#method for collecting sudent data from using and adding to hash.
+def student_input
+
+  puts "PLease enter the names of the enrolling students:"
+  puts "To finish, press enter twice"
+  #now for the empty array
+  students = []
+  #for first name
+  name = gets.chomp
+  #while name isn't empty, repeat.
+  while !name.empty? do
+    #now to add student hashe
+    students << {name: name, cohort: :march}
+    puts "Now we have #{students.count} eeeevillll students"
+    #gets another name
+    name = gets.chomp
+  end
+  #return array
+  students
+end
+
+def print_header
+puts "The Students of Villains Academy"
+puts "------------------------------"
+end
+
+def print(students)
+students.each do |student|
+  if student[:name].length < 12
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+else
+  puts "Invalid Name (too long)"
+end
+end
+end
+
+def print_footer(students)
+puts "There are exactly #{students.count} eeeevilll students."
+end
+#now to call methods
+students = student_input
+print_header
+print(students)
+puts
+print_footer(students)
