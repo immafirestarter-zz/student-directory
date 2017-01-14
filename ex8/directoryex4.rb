cd17bbf1ae6d1f49b367aee05c0afe8345248ffe
Sami <sami7mason@yahoo.co.uk>
2017-01-14 22:54:44 +0000

    ex4

---
+++ ex8/directoryex4.rb
@@ -0,0 +1,43 @@
+#method for collecting sudent data from using and adding to hash.
+def student_input
+
+  puts "PLease enter the names of the enrolling students:"
+  puts "To finish, press enter twice"
+  #now for the empty array
+  students = []
+  #for first name
+  name = gets.chomp
+  #while name isn't empty, repeat.
+  while !name.empty? do
+    #now to add student hashe
+    students << {name: name, cohort: :march}
+    puts "Now we have #{students.count} eeeevillll students"
+    #gets another name
+    name = gets.chomp
+  end
+  #return array
+  students
+end
+
+def print_header
+puts "The Students of Villains Academy"
+puts "------------------------------"
+end
+
+def print(students)
+a = 0
+while a < students.length #giving condition for the counter
+  puts "#{students[a][:name]} (#{students[a][:cohort]} cohort)"
+a = a + 1
+end
+end
+
+def print_footer(students)
+puts "There are exactly #{students.count} eeeevilll students."
+end
+#now to call methods
+students = student_input
+print_header
+print(students)
+puts
+print_footer(students)
